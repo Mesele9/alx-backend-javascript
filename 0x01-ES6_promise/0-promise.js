83f04ed87441509a51695fc619af707446f13c03
Mesele Assefa <getmesy@gmail.com>
2023-07-02 14:34:20 +0000

    push task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve(true);
+    } else {
+      reject(Error);
+    }
+  });
+}
+
+export default getResponseFromAPI;
