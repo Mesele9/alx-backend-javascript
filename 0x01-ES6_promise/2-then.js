914f6ac51097ac222f4af134cdd767cef17ce97b
Mesele Assefa <getmesy@gmail.com>
2023-07-02 16:25:33 +0000

    correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ function handleResponseFromAPI(promise) {
     console.log('Got a response from the API');
     return {
       status: 200,
-      body: 'success',
+      body: 'success'
     };
   })
     .catch(() => new Error());
