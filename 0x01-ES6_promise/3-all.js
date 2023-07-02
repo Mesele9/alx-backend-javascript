017eb1706fc13bae3fa1dda5c6a83cbfedac0b5d
Mesele Assefa <getmesy@gmail.com>
2023-07-02 17:58:09 +0000

    push task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { createUser, uploadPhoto } from './utils';
+
+function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  Promise.all([photoPromise, userPromise])
+    .then(([photoPromise, userPromise]) => {
+      console.log(`${photoPromise.body} ${userPromise.firstName} ${userPromise.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
