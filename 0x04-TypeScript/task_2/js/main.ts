a1aa4cbd6dbb478cca982d898bcb9552e85c2a28
Mesele Assefa <getmesy@gmail.com>
2023-07-07 03:06:18 +0000

    push task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -11,41 +11,31 @@ interface TeacherInterface {
 }
 
 class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
-
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";      
-  }
-
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
+  workFromHome = () => 'Working from home';
+  getCoffeeBreak = () => 'Getting a coffee break';      
+  workDirectorTasks = () => 'Getting to director tasks';
 }
 
 class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
-  }
-
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
-  }
-
-  workTeacherTasks(): string {
-    return "Getting to work";
-  }
+  workFromHome = () => 'Cannot work from home';
+  getCoffeeBreak = () => 'Cannot have a break';
+  workTeacherTasks = () => 'Getting to work';
 }
 
-export function createEmployee(salary: number | string) {
-  if (typeof salary === "number" && salary < 500) {
+function createEmployee(salary: number | string) {
+  if (typeof salary === 'number' && salary < 500)
     return new Teacher();
-  } else {
-    return new Director();
-  }
+  return new Director();
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee("$500"));
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
