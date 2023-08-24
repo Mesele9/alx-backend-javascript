88abfba5bcd50aa1eb3a5d727b70843e4766154f
Mesele Assefa <getmesy@gmail.com>
2023-08-24 13:28:12 +0000

    separate the tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,8 +4,17 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', function() {
   it('Should return the sum of rounded numbers', function() {
     assert.equal(calculateNumber(1, 3), 4);
+  });
+
+  it('Should return the sum of rounded numbers', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
+  });
+
+  it('Should return the sum of rounded numbers', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('Should return the sum of rounded numbers', function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
