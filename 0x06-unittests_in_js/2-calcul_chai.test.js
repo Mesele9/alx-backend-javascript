0fa336786eccb743b2255b46da9723c72a297f86
Mesele Assefa <getmesy@gmail.com>
2023-08-24 14:05:10 +0000

    done task2

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', function() {
+  it('Checks equality', function() {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+  
+   it('Checks equality', function() {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+   });
+
+   it('Checks equality', function() {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+   });
+
+   it('Checks equality', function() {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal("Error");
+  });
+});
