13bfa844291a7fe1dec65b98d6ac8b9d9484c68c
Mesele Assefa <getmesy@gmail.com>
2023-08-24 14:13:01 +0000

    correction

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,18 +4,22 @@ const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', function() {
   it('Checks equality', function() {
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    const result = calculateNumber('SUM', 1.4, 4.5);
+    expect(result).to.equal(6);
   });
   
    it('Checks equality', function() {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    const result = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(result).to.equal(-4);
    });
 
    it('Checks equality', function() {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    const result = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(result).to.equal(0.2);
    });
 
    it('Checks equality', function() {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal("Error");
+    const result = calculateNumber('DIVIDE', 1.4, 0);
+    expect(result).to.equal("Error");
   });
 });
