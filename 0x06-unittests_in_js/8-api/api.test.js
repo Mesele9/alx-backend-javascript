97c05970785420516f9a17bec53775919546f4c7
Mesele Assefa <getmesy@gmail.com>
2023-08-25 00:40:01 +0000

    push task8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,24 @@
+const expect = require('chai').expect;
+const request = require('request');
+const server = require('./api');
+
+describe('Index page', function() {
+  it('Should return correct status code', function(done) {
+    request.get('http://localhost:7865', function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Should return correct result', function(done) {
+    request.get('http://localhost:7865', function(error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+after(function() {
+  server.close();
+});
+
