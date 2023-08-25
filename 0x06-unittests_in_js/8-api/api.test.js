483fec3895f8d9c01c189c1c18ff4057e3a7bc84
Mesele Assefa <getmesy@gmail.com>
2023-08-25 18:12:57 +0000

    correction

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,15 +3,16 @@ const request = require('request');
 const app = require('./api');
 
 describe('Index page', function() {
+  const url = 'http://localhost:7865'
   it('Should return correct status code', function(done) {
-    request('http://localhost:7865', function(error, response, body) {
+    request(url, function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('Should return correct result', function(done) {
-    request('http://localhost:7865', function(error, response, body) {
+    request(url, function(error, response, body) {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
--- 0x06-unittests_in_js/8-api/test.js
+++ 0x06-unittests_in_js/8-api/test.js
@@ -2,13 +2,13 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('Index page', () => {
-	  const endpoint = 'http://localhost:7865';
+  const endpoint = 'http://localhost:7865';
 
-	  it('Returns the right content', function (done) {
-		      request(endpoint, function (error, response, body) {
-			            expect(body).to.equal('Welcome to the payment system');
-			            done();
-			          });
-		    });
+  it('Returns the right content', function (done) {
+    request(endpoint, function (error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+ });
 
 });
